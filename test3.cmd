1e48747930bebf3825a7079853af6492c22d5c9a
kanebro71 <95398322+kanebro71@users.noreply.github.com>
2021-12-02 09:11:32 +0400

    Add files via upload

---
+++ test3.cmd
@@ -0,0 +1,74 @@
+@echo off
+echo                             ABOUT----ABOUT-----ABOUT-----KANE10
+echo                             ABOUT----ABOUT-----ABOUT-----KANE10
+echo                             ABOUT----ABOUT-----ABOUT-----KANE10
+echo                             ABOUT----ABOUT-----ABOUT-----KANE10
+echo `
+echo `
+echo `
+echo [[[[[[[                 [[[[[[[                     [[[[[[[ 
+echo [[[[[[[                [[[[[[[                     [[[[[[[                              
+echo [[[[[[[               [[[[[[[                     [[[[[[[                             
+echo [[[[[[[              [[[[[[[                     [[[[[[[                             
+echo [[[[[[[             [[[[[[[                     [[[[[[[                              
+echo [[[[[[[            [[[[[[[                     [[[[[[[                             
+echo [[[[[[[           [[[[[[[                     [[[[[[[                           
+echo [[[[[[[          [[[[[[[                     [[[[[[[                           
+echo [[[[[[[         [[[[[[[                     [[[[[[[                         
+echo [[[[[[[        [[[[[[[                     [[[[[[[                           
+echo [[[[[[[       [[[[[[[                     [[[[[[[                             
+echo [[[[[[[      [[[[[[[                     [[[[[[[                              
+echo [[[[[[[     [[[[[[[                     [[[[[[[                ????????????????
+echo [[[[[[[    [[[[[[[                      [[[[[[[                ????????????????		         
+echo [[[[[[[   [[[[[[[                     [[[[[[[                  ????????????????
+echo [[[[[[[   [[[[[[[                     [[[[[[[                  ????????????????		         
+echo [[[[[[[  [[[[[[[                     [[[[[[[                   ????????????????
+echo [[[[[[[  [[[[[[[                     [[[[[[[                   ????????????????		      
+echo [[[[[[[ [[[[[[[                     [[[[[[[                    ???	    ???       
+echo [[[[[[[[[[[[[[                      [[[[[[[                    ??? 	    ???      
+echo [[[[[[[ [[[[[[[                     [[[[[[[                    ??? 	    ???   
+echo [[[[[[[  [[[[[[[                     [[[[[[[                   ??? 	    ???                           
+echo [[[[[[[   [[[[[[[                     [[[[[[[                  ???	    ???    
+echo [[[[[[[    [[[[[[[                     [[[[[[[                 ???	    ???
+echo [[[[[[[     [[[[[[[                     [[[[[[[                ??? 	    ???
+echo [[[[[[[      [[[[[[[                     [[[[[[[               ???	    ???
+echo [[[[[[[       [[[[[[                      [[[[[[[              ???	    ???
+echo [[[[[[[        [[[[[[[                     [[[[[[[             ???	    ???
+echo [[[[[[[         [[[[[[[                     [[[[[[[            ???	    ???
+echo [[[[[[[          [[[[[[[                     [[[[[[[           ???	    ???
+echo [[[[[[[           [[[[[[[                     [[[[[[[          ???          ???
+echo [[[[[[[            [[[[[[[                     [[[[[[[         ???	    ???
+echo [[[[[[[             [[[[[[[                     [[[[[[[        ????????????????		
+echo [[[[[[[              [[[[[[[                     [[[[[[[       ????????????????  		
+echo [[[[[[[               [[[[[[[                     [[[[[[[      ????????????????
+echo [[[[[[[               [[[[[[[                     [[[[[[[      ????????????????
+echo `
+echo ` 
+echo ` 
+
+echo 		kane10  //k10// kane10 kan10 kane10 kan10 kane10	
+echo 		kane10  //k10// kane10 kan10 kane10 kan10 kane10	
+echo 		kane10  //k10// kane10 kan10 kane10 kan10 kane10	
+echo 		kane10  //k10// kane10 kan10 kane10 kan10 kane10	
+echo 		kane10  //k10// kane10 kan10 kane10 kan10 kane10	
+echo 		kane10  //k10// kane10 kan10 kane10 kan10 kane10	
+echo 		kane10  //k10// kane10 kan10 kane10 kan10 kane10		
+           
+
+echo `
+echo `
+echo `
+echo 		GRAPHICS-DESIGN:PRABATH_KUMARA
+echo 		STORY        -KANE
+echo 		IDEA           -JASON_KERANSKI
+echo 		GAME-ENGINE          -KANE10GIN.POP
+echo ------------------------------------------kANE10 allrights reserved----------------
+echo ------------------------------------------kANE10 allrights reserved----------------
+echo ------------------------------------------kANE10 allrights reserved----------------
+:c
+:c
+:c
+color c
+goto c
+   
+pause
